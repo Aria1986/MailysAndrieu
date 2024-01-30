45e12ab322dba8ca5a8bba1816640ff9ef43e6a7
maimai <mailys.andrieu.simplon@gmail.com>
2024-01-30 22:58:09 +0100

    version 30/01/24 nuage footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,12 @@ const carte = document.getElementById('projetCarte');
 const femme = document.getElementById('woman');
 
 pointeurCarte.addEventListener('click',afficherProjet)
-femme.addEventListener('click',afficherProjet)
+
 
 function afficherProjet(event){
     console.log('afficher projet1')
     carte.style.display='block';
+    const trait =document.createElement('div');
+    trait.classList.add('trait')
+    pointeurCarte.appendChild(trait)
 }
